f4b764121b26437571fda2974ef438c2f9ef3439
mariusciurea <ciurea.marius1@gmail.com>
2023-02-06 23:02:39 +0200

    models and specific templates created

--- programming_topics/topics/views.py
+++ programming_topics/topics/views.py
@@ -1,38 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Topic, Subtopic
 
 # Create your views here.
-dummy_data = [
-    {
-        'id': 1,
-        'name': 'Lets learn python'
-    },
-    {
-        'id': 2,
-        'name': 'Lets learn HTML'
-    },
-    {
-        'id': 3,
-        'name': 'What about JavaScript?'
-    }
-]
+# dummy_data = [
+#     {
+#         'id': 1,
+#         'name': 'Lets learn python'
+#     },
+#     {
+#         'id': 2,
+#         'name': 'Lets learn HTML'
+#     },
+#     {
+#         'id': 3,
+#         'name': 'What about JavaScript?'
+#     }
+# ]
 
 
 def home(request):
     # return HttpResponse("""<h1>Welcome to my home page</h1>
     #                         <p>daslk;fjaoksdfhaisdhfiajseghfjhasgf</p>""")
-    context = {'data': dummy_data}
+    topics = Topic.objects.all()
+    print(topics)
+    context = {'topics': topics}
     return render(request, 'topics/home.html', context)
 
 
 def subtopics(request, pk):
-    data = None
-    for item in dummy_data:
-        if item['id'] == int(pk):
-            data = item
-    context = {'data': data}
-    print(data)
+    topic = Topic.objects.get(id=pk)
+    subtopics = Subtopic.objects.filter(topic=pk)
+    context = {'subtopics': subtopics, 'topic': topic}
     return render(request, 'topics/subtopics.html', context)
+
+
+
+def post(request, pk):
+    post = Subtopic.objects.get(id=pk)
+    context = {'post': post}
+    return render(request, 'topics/post.html', context)
 
 
 def about(request):
